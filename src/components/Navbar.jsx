754b1177e76f3f54c103fba20825dd80b9df1bcc
kwamedanso <dansokwame96@gmail.com>
2024-01-22 07:10:02 -0600

    Add Navigation Bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import navLinks from 'consts/navbar'
+import profile from "assets/profile.jpg"
+import "components/styles/navbar.css"
+
+export default function Navbar() {
+    const [hamburger, setHamburger] = useState(false)
+
+
+    return (
+        <>
+            <header className='flex jc-space-between ai-center'>
+                <div className="header-logo">
+                    <Link to={"/"}>
+                        <img src={profile} alt="Profile pic" />
+                    </Link>
+                </div>
+
+                <nav className={`nav-links ${hamburger ? "active" : null}`}>
+                    <ul className='flex jc-space-between'>
+                        {navLinks.map(item => (<li key={item.title} onClick={() => setHamburger(!hamburger)}><a className='fw-semi-bold' href={`#${item.title}`}>{item.title}</a></li>))}
+                    </ul>
+                </nav>
+
+                <div className={`hamburger ${hamburger ? "active" : null}`} onClick={() => setHamburger(!hamburger)}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+            </header>
+        </>
+    )
+}
---
+++ src/consts/navbar.js
@@ -0,0 +1,16 @@
+const navLinks = [
+    {
+        title: "Home"
+    },
+    {
+        title: "About"
+    },
+    {
+        title: "Projects"
+    },
+    {
+        title: "Contact"
+    }
+]
+
+export default navLinks;
