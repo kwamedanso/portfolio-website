49877eeb7d15e3d30d28f256c236f5eba1df4779
kwamedanso <dansokwame96@gmail.com>
2024-01-22 07:08:07 -0600

    Add Home, Resume and NotFound pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import About from 'components/About'
+import Hero from 'components/Hero'
+import Projects from 'components/Projects'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            {/* <About /> */}
+            {/* <Projects /> */}
+        </>
+    )
+}
