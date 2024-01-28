7bbf839af55e107e2ffeeff15983f5bf412ab19f
kwamedanso <dansokwame96@gmail.com>
2024-01-28 10:26:35 -0600

    Add Marquee(sliding text)

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { PiStarFourFill } from "react-icons/pi";
+
+
+export default function Marquee() {
+    return (
+        <div class="marquee">
+            <ul class="marquee__content">
+                <PiStarFourFill />
+                <li>Discover</li>
+                <PiStarFourFill />
+                <li>Design</li>
+                <PiStarFourFill />
+                <li>Develop</li>
+                <PiStarFourFill />
+            </ul>
+            {/* <!-- Mirrors the content above --> */}
+            <ul class="marquee__content" aria-hidden="true">
+                <li>Discover</li>
+                <PiStarFourFill />
+                <li>Design</li>
+                <PiStarFourFill />
+                <li>Develop</li>
+                <PiStarFourFill />
+            </ul>
+        </div>
+    )
+}
