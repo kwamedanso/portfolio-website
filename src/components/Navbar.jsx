051c05814c06ffd2486a7ff3e6807576aff2465b
kwamedanso <dansokwame96@gmail.com>
2024-01-28 10:36:02 -0600

    Add section margin to Navbar Increase navlinks font-size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
 
     return (
-        <>
+        <div className='section-margin'>
             <header className='flex jc-space-between ai-center'>
                 <div className="header-logo">
                     <Link to={"/"}>
@@ -19,7 +19,7 @@ export default function Navbar() {
 
                 <nav className={`nav-links ${hamburger ? "active" : null}`}>
                     <ul className='flex jc-space-between'>
-                        {navLinks.map(item => (<li key={item.title} onClick={() => setHamburger(!hamburger)}><a className='fw-semi-bold' href={`#${item.title}`}>{item.title}</a></li>))}
+                        {navLinks.map(item => (<li key={item.title} onClick={() => setHamburger(!hamburger)}><a className='fw-semi-bold fs-200' href={`#${item.title}`}>{item.title}</a></li>))}
                     </ul>
                 </nav>
 
@@ -29,6 +29,6 @@ export default function Navbar() {
                     <div className="bar"></div>
                 </div>
             </header>
-        </>
+        </div>
     )
 }
