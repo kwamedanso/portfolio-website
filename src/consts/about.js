0c8e1bcee772f4be85d24737fb1effdf509fb362
kwamedanso <dansokwame96@gmail.com>
2024-02-05 11:03:49 -0600

    Add about and workExperience constants

---
+++ src/consts/about.js
@@ -0,0 +1,5 @@
+const prof_skills = ["Time Management", "Team Collaboration", "Project Management", "Effective Communication", "Problem Solving", "Attention to Detail", "Continuous Learning and Adaptation", "User-Centric Mindset"]
+
+const tech_skills = ["HTML/CSS", "JavaScript", "React.js", "Redux", "Responsive Design", "Version Control with Git", "API Integration", "Testing with Jest", "Debugging", "Command Line Tools", "Cross-Browser Compatibility", "Web Performance Optimization", "Chrome dev tools", "React dev tools", "Web Accessibility(WCAG)", "UI/UX Design(Figma)", "Responsively App"]
+
+export { prof_skills, tech_skills }
---
+++ src/consts/workExperience.js
@@ -0,0 +1,26 @@
+const organization = [
+    {
+        id: 1,
+        org: "Self-Employed Developer",
+        date: "Sep 2016 - Aug 2011",
+        jtitle: "Visual Designer",
+        description: "A visual designer for a variety of platforms, which may include internet and intranet sites, games, movies, kiosks and wearables. In short, they create the concepts."
+    },
+    {
+        id: 2,
+        org: "New Man Services",
+        date: "Sep 2016 - Aug 2014",
+        jtitle: "Visual Designer",
+        description: "A visual designer for a variety of platforms, which may include internet and intranet sites, games, movies, kiosks and wearables. In short, they create the concepts."
+    },
+    {
+        id: 3,
+        org: "Global solutions",
+        date: "Sep 2016 - Aug 2014",
+        jtitle: "Visual Designer",
+        description: "A visual designer for a variety of platforms, which may include internet and intranet sites, games, movies, kiosks and wearables. In short, they create the concepts."
+    }
+]
+
+
+export { organization };
