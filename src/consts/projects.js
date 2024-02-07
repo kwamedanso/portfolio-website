99259488ebd8183c87ba4c74b18edc4f63959b46
kwamedanso <dansokwame96@gmail.com>
2024-02-07 02:18:52 -0600

    Add info for each project(image, desctiption)

---
+++ src/consts/projects.js
@@ -0,0 +1,38 @@
+import techee from "assets/techee.jpeg"
+import travistTravel from "assets/travist-travel.png"
+
+const projects = [
+    {
+        id: 0,
+        title: "Car Rental",
+        description: "With the focus on simplicity and clean desing, this store prioritize user experience, making it easy for customers to find and purchase the products they need.",
+        madeWith: ["React.js", "CSS", "Supabse"],
+        repo: "https://www.google.com",
+        liveDemo: "https://techee.netlify.app",
+        imageUrl: techee,
+        position: "left"
+    },
+    {
+        id: 1,
+        title: "Carru Car Rentals",
+        description: "A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.",
+        madeWith: ["React.js", "CSS"],
+        repo: "https://www.google.com",
+        liveDemo: "https://techee.netlify.app",
+        imageUrl: travistTravel,
+        position: "right"
+    },
+    {
+        id: 2,
+        title: "Travist Travel",
+        description: "With the focus on simplicity and clean desing, this store prioritize user experience, making it easy for customers to find and purchase the products they need.",
+        madeWith: ["React.js", "CSS"],
+        repo: "https://www.google.com",
+        liveDemo: "https://travist-travel.netlify.app",
+        imageUrl: travistTravel,
+        position: "left"
+    }
+]
+
+
+export default projects;
