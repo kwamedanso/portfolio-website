17ba8e69f14e1b0c88da85ef1355ba14e4d4bd68
kwamedanso <dansokwame96@gmail.com>
2024-02-07 05:37:15 -0600

    Change Hero section background color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,36 +17,38 @@ export default function Hero() {
     }
 
     return (
-        <div className='section-margin'>
-            <div id='Home' className='hero'>
-                <div className="hero-content">
-                    <div className='hero-text'>
-                        <code className=''>Hello world - this is my portfolio page</code>
-                        <div className='margin-block-200'>
-                            <p className='fs-800 fw-bold'>My name is <span className='content-name'>Kwame</span></p>
-                            <p className='fs-800 fw-bold'>I'm a <span className='content-underline'>Front End  Developer</span></p>
-                        </div>
-                        <p className='fs-250 op-200 margin-block-300'>With 3+ years of experience in web design and development, my true skill development began during my intership at Ant Cyber Engineering. This experience has porpelled my ongoing journey of learbing and constant improvement.
-                        </p>
-                        <div className="hero-links">
-                            <Link target='_blank' to={"https://github.com/kwamedanso"}><FaGithub /></Link>
-                            <Link target='_blank' to={"https://www.linkedin.com/in/kwame-amoafo-danso-4b07072aa/"}><FaLinkedin /></Link>
+        <div id='Home'>
+            <div className='section-margin'>
+                <div className='hero'>
+                    <div className="hero-content">
+                        <div className='hero-text'>
+                            <code className=''>Hello world - this is my portfolio page</code>
+                            <div className='margin-block-200'>
+                                <p className='fs-800 fw-bold'>My name is <span className='content-name'>Kwame</span></p>
+                                <p className='fs-800 fw-bold'>I'm a <span className='content-underline'>Front End  Developer</span></p>
+                            </div>
+                            <p className='fs-250 op-200 margin-block-300'>With 3+ years of experience in web design and development, my true skill development began during my intership at Ant Cyber Engineering. This experience has porpelled my ongoing journey of learbing and constant improvement.
+                            </p>
+                            <div className="hero-links">
+                                <Link target='_blank' to={"https://github.com/kwamedanso"}><FaGithub /></Link>
+                                <Link target='_blank' to={"https://www.linkedin.com/in/kwame-amoafo-danso-4b07072aa/"}><FaLinkedin /></Link>
 
+                            </div>
+                        </div>
+
+                        <div className={`resume-links ${resumeLinks ? "active" : ""}`}>
+                            <Link to={"/resume"} className='button resume-link' data-type='white'>View <BsEye /></Link>
+                            <a href={resumeFile} target='_blank' rel='noreferrer' className='button resume-link' data-type='white' download={"KwameDanso-resume.pdf"}>Download <MdOutlineFileDownload /></a>
+                        </div>
+                        <div className="hero-buttons">
+                            <button className='button' data-type='white' onClick={handleResumeLinks}>Resume <PiScrollLight /></button>
+                            <a href='#Contact' className='button' data-type='blue'>Contact me <MdOutlineArrowOutward /></a>
                         </div>
                     </div>
 
-                    <div className={`resume-links ${resumeLinks ? "active" : ""}`}>
-                        <Link to={"/resume"} className='button resume-link' data-type='white'>View <BsEye /></Link>
-                        <a href={resumeFile} target='_blank' rel='noreferrer' className='button resume-link' data-type='white' download={"KwameDanso-resume.pdf"}>Download <MdOutlineFileDownload /></a>
-                    </div>
-                    <div className="hero-buttons">
-                        <button className='button' data-type='white' onClick={handleResumeLinks}>Resume <PiScrollLight /></button>
-                        <a href='#Contact' className='button' data-type='blue'>Contact me <MdOutlineArrowOutward /></a>
-                    </div>
+                    <div className={`overlay ${resumeLinks ? "active" : ""}`} onClick={handleResumeLinks}></div>
+
                 </div>
-
-                <div className={`overlay ${resumeLinks ? "active" : ""}`} onClick={handleResumeLinks}></div>
-
             </div>
         </div>
     )
