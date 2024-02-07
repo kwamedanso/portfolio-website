fd9b0f077d7de5d08cb676960ba3b6c73c30401a
kwamedanso <dansokwame96@gmail.com>
2024-02-07 02:11:43 -0600

    Update Hero section component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,9 +22,11 @@ export default function Hero() {
                 <div className="hero-content">
                     <div className='hero-text'>
                         <code className=''>Hello world - this is my portfolio page</code>
-                        <p className='fs-800 fw-bold'>My name is <span className='content-name'>Kwame</span></p>
-                        <p className='fs-800 fw-bold'>I'm a <span className='content-underline'>Front End  Developer</span></p>
-                        <p className='fs-250 op-200 margin-block-200'>With 3+ years of experience in web design and development, my true skill development began during my intership at Ant Cyber Engineering. This experience has porpelled my ongoing journey of learbing and constant improvement.
+                        <div className='margin-block-200'>
+                            <p className='fs-800 fw-bold'>My name is <span className='content-name'>Kwame</span></p>
+                            <p className='fs-800 fw-bold'>I'm a <span className='content-underline'>Front End  Developer</span></p>
+                        </div>
+                        <p className='fs-250 op-200 margin-block-300'>With 3+ years of experience in web design and development, my true skill development began during my intership at Ant Cyber Engineering. This experience has porpelled my ongoing journey of learbing and constant improvement.
                         </p>
                         <div className="hero-links">
                             <Link target='_blank' to={"https://github.com/kwamedanso"}><FaGithub /></Link>
@@ -35,7 +37,7 @@ export default function Hero() {
 
                     <div className={`resume-links ${resumeLinks ? "active" : ""}`}>
                         <Link to={"/resume"} className='button resume-link' data-type='white'>View <BsEye /></Link>
-                        <a href={resumeFile} target='_blank' className='button resume-link' data-type='white' download={"KwameDanso-resume.pdf"}>Download <MdOutlineFileDownload /></a>
+                        <a href={resumeFile} target='_blank' rel='noreferrer' className='button resume-link' data-type='white' download={"KwameDanso-resume.pdf"}>Download <MdOutlineFileDownload /></a>
                     </div>
                     <div className="hero-buttons">
                         <button className='button' data-type='white' onClick={handleResumeLinks}>Resume <PiScrollLight /></button>
