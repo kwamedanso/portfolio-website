f5c073317565bb216de2e700c7bdedad00916ae3
kwamedanso <dansokwame96@gmail.com>
2024-05-14 09:21:09 -0500

    Update work experience data.

--- src/consts/workExperience.js
+++ src/consts/workExperience.js
@@ -1,24 +1,54 @@
 const organization = [
     {
         id: 1,
-        org: "Self-Employed Developer",
-        date: "Sep 2016 - Aug 2011",
-        jtitle: "Visual Designer",
-        description: "A visual designer for a variety of platforms, which may include internet and intranet sites, games, movies, kiosks and wearables. In short, they create the concepts."
+        org: "Ant Cyber Engineering Limited",
+        date: "January 2022 - Present",
+        jtitle: "Frontend Web Developer",
+        experience: [
+            "Develop responsible web pages and interfaces using HTML5, CSS3, JavaScript, and React.js",
+            "Improved website accessibility by implementing responsive design, resulting in a 15% increase in mobile traffic.",
+            "Implement state management tools e.g. Redux to manage the application state data effectively.",
+            "Work closely with backend developers to understand and integrate APIs and data interchange.",
+            "Optimized website performance using asynchronous loading, and image optimization, resulting in a 20% decrease in page load time.",
+            "Use image compression tools to reduce file sizes without compromising quality.",
+            "Minify CSS, JavaScript, and HTML. Remove unnecessary characters from code to reduce file sizes, using tools like UglifyJS and CSSNano.",
+            "Enable browser cashing, allowing browsers to store copies of files so they don‚Äôt need to be downloaded again on subsequent visits.",
+            "Use browser development tools for debugging and optimization (Chrome Dev Tools, React Dev Tools).",
+            "Use version control systems, like Git, for source code management."
+        ]
     },
     {
         id: 2,
-        org: "New Man Services",
-        date: "Sep 2016 - Aug 2014",
-        jtitle: "Visual Designer",
-        description: "A visual designer for a variety of platforms, which may include internet and intranet sites, games, movies, kiosks and wearables. In short, they create the concepts."
+        org: "Ant Cyber Engineering Limited",
+        date: "June 2021 - December 2021",
+        jtitle: "Frontend Web Developer (Intern)",
+        experience: ["Develop responsible web pages and interfaces using HTML5, CSS3, JavaScript.", "Use browser development tools for debugging and optimization.", "Refractor and optimize the codebase for maintainability and scalability.", "Assisted in the integration of third-party APIs, enhancing the functionality and interactivity of the company's web applications", "Modified the code to make websites more responsive on mobile devices and compatible with more browsers.", "25% more user engagement was achieved by adding interactive capabilities to the company's web apps using JavaScript, HTML, and CSS.", "Use version control systems, like Git, for source code management."]
     },
     {
         id: 3,
-        org: "Global solutions",
-        date: "Sep 2016 - Aug 2014",
+        org: "Ernest Chemists Limited",
+        date: "September 2019 - December 2020",
         jtitle: "Visual Designer",
-        description: "A visual designer for a variety of platforms, which may include internet and intranet sites, games, movies, kiosks and wearables. In short, they create the concepts."
+        experience: [
+            "Provide first-line support to internal users, resolving issues related to software, hardware, and network systems.",
+            "Diagnose and troubleshoot technical issues, including errors, network configurations, and hardware malfunctions.",
+            "Manage help desk tickets, responding in a timely manner to service issues and requests.",
+            "Provide remote support to users for certain issues, utilizing remote desktop connections to access systems directly.",
+            "Perform regular checks and maintenance on network and data security measures.",
+            "Schedule and execute system upgrades and backups to prevent data loss and ensure efficient operations.",
+            "Maintain IT inventory, including hardware, software, and various licenses, ensuring they are up to date.",
+            "Manage network servers and technology tools, ensuring seamless connectivity and infrastructure performance.",
+            "Install and update antivirus software to ensure protection from malware and other security threats.",
+            "Educate and train users on security protocols and best practices to mitigate risks.",
+            "Document technical knowledge in the form of notes and manuals, maintaining detailed records of system configurations and operations.",
+            "Communicate effectively with non - technical staff, providing clear instructions and feedback regarding IT systems and policies.",
+            "Participated in IT projects(iVend Retail Management Software rollout), contributing to the planning, execution and completion of specific IT objectives.",
+            "Managed iVend Retail Management Software with AWS servers.",
+            "Trained and supervised staff in iVend and Tally software.",
+            "Evaluate new technology and systems, recommending upgrades or changes to improve IT efficiency and performance.",
+            "Negotiate with vendors to secure cost - effective solutions and services for organization."
+
+        ]
     }
 ]
 
