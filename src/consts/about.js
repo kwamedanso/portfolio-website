ff5309a30fb322a4590d6bf25b31e82e38d87262
kwamedanso <dansokwame96@gmail.com>
2024-05-14 09:22:49 -0500

    Add education const to be used by the About component.

--- src/consts/about.js
+++ src/consts/about.js
@@ -2,4 +2,17 @@ const prof_skills = ["Time Management", "Team Collaboration", "Project Managemen
 
 const tech_skills = ["HTML/CSS", "JavaScript", "React.js", "Redux", "Responsive Design", "Version Control with Git", "API Integration", "Testing with Jest", "Debugging", "Command Line Tools", "Cross-Browser Compatibility", "Web Performance Optimization", "Chrome dev tools", "React dev tools", "Web Accessibility(WCAG)", "UI/UX Design(Figma)", "Responsively App"]
 
-export { prof_skills, tech_skills }
+const education = [
+    {
+        cert: "Bachelor of Technology (Computer Science)",
+        school: "Accra Technical University",
+        year: "January 2021 - December 2022"
+    },
+    {
+        cert: "Higher National Diploma (Computer Science)",
+        school: "Accra Technical University",
+        year: "September 2016 - November 2019"
+    }
+]
+
+export { prof_skills, tech_skills, education }
