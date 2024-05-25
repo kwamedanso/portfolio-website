00aaebacfaad1466e2822b923f15ea6b20ff621d
kwamedanso <dansokwame96@gmail.com>
2024-05-25 12:20:17 -0500

    Change links to git repos and project titles

--- src/consts/projects.js
+++ src/consts/projects.js
@@ -5,10 +5,10 @@ import carru from "assets/carru.png"
 const projects = [
     {
         id: 0,
-        title: "Car Rental",
+        title: "Techee e-commerce shop",
         description: "With the focus on simplicity and clean desing, this store prioritize user experience, making it easy for customers to find and purchase the products they need.",
         madeWith: ["React.js", "CSS", "Supabse"],
-        repo: "https://www.google.com",
+        repo: "https://github.com/kwamedanso/techee",
         liveDemo: "https://techee.netlify.app",
         imageUrl: techee,
         position: "left"
@@ -18,7 +18,7 @@ const projects = [
         title: "Carru Car Rentals",
         description: "A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.",
         madeWith: ["React.js", "CSS"],
-        repo: "https://www.google.com",
+        repo: "https://github.com/kwamedanso/carru-car-rental.git",
         liveDemo: "https://techee.netlify.app",
         imageUrl: carru,
         position: "right"
@@ -28,7 +28,7 @@ const projects = [
         title: "Travist Travel",
         description: "With the focus on simplicity and clean desing, this store prioritize user experience, making it easy for customers to find and purchase the products they need.",
         madeWith: ["React.js", "CSS"],
-        repo: "https://www.google.com",
+        repo: "https://github.com/kwamedanso/travist-travel",
         liveDemo: "https://travist-travel.netlify.app",
         imageUrl: travistTravel,
         position: "left"
