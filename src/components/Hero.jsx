4c76d22806c33cf247eada4439c5f82ebbbb9909
kwamedanso <dansokwame96@gmail.com>
2024-06-06 10:57:02 -0500

    Add UI animations and update contents of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,40 +2,56 @@ import React from 'react'
 import "components/styles/hero.css"
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import { MdOutlineFileDownload, MdOutlineArrowOutward } from "react-icons/md";
 import resumeFile from "assets/Kwame-Danso-resume.pdf";
+import { motion } from 'framer-motion';
+import { childVariant, containerVariant } from './framerVariants';
 
 
 
 export default function Hero() {
     return (
-        <div id='Home'>
+        <motion.div variants={containerVariant} animate="visible" initial="hidden" id='Home'>
             <div className='section-margin'>
                 <div className='hero'>
                     <div className="hero-content">
                         <div className='hero-text'>
-                            <code className='fw-semi-bold'>Hello world - this is my portfolio page</code>
-                            <div className='margin-block-200'>
-                                <p className='fs-800 fw-bold'>My name is <span className='content-name'>Kwame</span></p>
-                                <p className='fs-800 fw-bold'>I'm a <span className='content-underline'>Front End  Developer</span></p>
-                            </div>
-                            <p className='fs-250 op-200 margin-block-300 fw-semi-bold'>With over 4 years of experience in web design and development, my true skill development began during my intership at Ant Cyber Engineering Limited. This experience has porpelled my ongoing journey of learbing and constant improvement.
-                            </p>
-                            <div className="hero-links">
-                                <Link target='_blank' to={"https://github.com/kwamedanso"}><FaGithub /></Link>
-                                <Link target='_blank' to={"https://www.linkedin.com/in/kwame-amoafo-danso-4b07072aa/"}><FaLinkedin /></Link>
+                            <motion.code variants={childVariant} className='fw-semi-bold fs-200'>Hello world! - this is my portfolio page.</motion.code>
 
-                            </div>
+                            <motion.div variants={childVariant}>
+                                <div className='margin-block-200'>
+                                    <div className="primary-heading">
+                                        <p className='fw-bold'>
+                                            My name is
+                                            <span className='content-name'> Kwame</span>
+                                        </p>
+                                        <p className='fw-bold'>
+                                            Front-End
+                                            <span className='less-than'> {'<'}</span>
+                                            <span className='developer'>Developer</span>
+                                            <span className='greater-than'>{'>'}</span>
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className='padding-block-100'>{'{'}React, JavaScript, HTML/CSS, Git, RestAPI{'...}'}</p>
+                                        <p className='padding-block-200'>Specializing in React & JavaScript, I leverage cutting edge technologies to bring web projects to life.</p>
+                                    </div>
+                                </div>
+
+                                <div className="hero-links">
+                                    <Link target='_blank' to={"https://github.com/kwamedanso"}><FaGithub /></Link>
+                                    <Link target='_blank' to={"https://www.linkedin.com/in/kwame-amoafo-danso-4b07072aa/"}><FaLinkedin /></Link>
+                                </div>
+                            </motion.div>
                         </div>
 
 
-                        <div className="hero-buttons">
-                            <a href={resumeFile} target='_blank' rel='noreferrer' className='button' data-type="white" download={"KwameDanso-resume.pdf"}>Resume <MdOutlineFileDownload /></a>
-                            <a href='#Contact' className='button' data-type='blue'>Contact me <MdOutlineArrowOutward /></a>
-                        </div>
+                        <motion.div variants={childVariant} className="hero-buttons">
+                            <a href='#Contact' className='button' data-type='blue'>Let's Talk</a>
+                            <a href={resumeFile} target='_blank' rel='noreferrer' className='button' data-type="white" download={"KwameDanso-resume.pdf"}>View Resume</a>
+                        </motion.div>
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
