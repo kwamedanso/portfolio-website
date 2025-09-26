60c9d11ff1394c5234eb8c4db2e389464ce3608a
kwamedanso <dansokwame96@gmail.com>
2025-09-26 11:44:25 -0500

    Center the navbar component to the center in a pill shape

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,28 +12,30 @@ export default function Navbar() {
 
 
     return (
-        <motion.div className='section-margin'>
-            <header className='flex jc-space-between ai-center'>
-                <motion.div animate={{ x: 0 }} initial={{ x: '-100px' }} className="header-logo">
-                    <Link to={"/"}>
-                        <img src={profile} alt="Profile pic" />
-                    </Link>
-                </motion.div>
-
-                <nav className={`nav-links ${hamburger ? "active" : null}`}>
-                    <motion.div variants={navVariant} initial="hidden" animate="visible" >
-                        <ul className='flex jc-space-between'>
-                            {navLinks.map(item => (<li key={item.title} onClick={() => setHamburger(!hamburger)}><a className='fw-semi-bold fs-200' href={`#${item.title}`}>{item.title}</a></li>))}
-                        </ul>
+        <div className="navbar-container">
+            <motion.div className='section-margin'>
+                <header className='flex jc-space-between ai-center'>
+                    <motion.div animate={{ x: 0 }} initial={{ x: '-100px' }} className="header-logo">
+                        <Link to={"/"}>
+                            <img src={profile} alt="Profile pic" />
+                        </Link>
                     </motion.div>
-                </nav>
 
-                <div className={`hamburger ${hamburger ? "active" : null}`} onClick={() => setHamburger(!hamburger)}>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                </div>
-            </header>
-        </motion.div>
+                    <nav className={`nav-links ${hamburger ? "active" : null}`}>
+                        <motion.div variants={navVariant} initial="hidden" animate="visible" >
+                            <ul className='flex jc-space-between'>
+                                {navLinks.map(item => (<li key={item.title} onClick={() => setHamburger(!hamburger)}><a className='fw-semi-bold fs-200' href={`#${item.title}`}>{item.title}</a></li>))}
+                            </ul>
+                        </motion.div>
+                    </nav>
+
+                    <motion.div animate={{ x: 0 }} initial={{ x: '100px' }} transition={{ delay: .4 }} className={`hamburger ${hamburger ? "active" : null}`} onClick={() => setHamburger(!hamburger)}>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                        <div className="bar"></div>
+                    </motion.div>
+                </header>
+            </motion.div>
+        </div>
     )
 }
